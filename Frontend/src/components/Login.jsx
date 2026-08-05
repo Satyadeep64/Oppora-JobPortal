28822b84b003f76c81c9384f37320954015b52d1
Satyadeep Srivastava <satyadeepsrivastava3@gmail.com>
2026-08-05 15:27:28 +0530

    Import missing API_BASE_URL in Login.jsx

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,6 +1,7 @@
 import { useState,useEffect} from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { API_BASE_URL } from "../config/api";
 
 const Login = ({ setLoading }) => {
 
